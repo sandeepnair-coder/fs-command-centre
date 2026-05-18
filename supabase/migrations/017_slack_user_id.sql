6df348743bb1355ae3cd3269bf6a9ad38802e21a
Sohail Hatim <sohailhatim@gofynd.com>
2026-05-18 14:07:38 +0530

    revert: pause Slack identity schema changes

--- supabase/migrations/017_slack_user_id.sql
+++
@@ -1,3 +0,0 @@
--- Add Slack user ID to members for agent write attribution
-ALTER TABLE members ADD COLUMN IF NOT EXISTS slack_user_id text UNIQUE;
-CREATE INDEX IF NOT EXISTS idx_members_slack_user_id ON members(slack_user_id);
